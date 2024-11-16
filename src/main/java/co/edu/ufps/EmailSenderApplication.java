def10ccbb55804f0caa4bb5fe47c460d034f8c50
andresnico44 <ramiroandresnc@ufps.edu.co>
2024-11-16 15:46:05 -0500

    Email Sender Backend

--- src/main/java/co/edu/ufps/EmailSenderApplication.java
+++ src/main/java/co/edu/ufps/EmailSenderApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmailSenderApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmailSenderApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSenderApplication.class, args);
+    }
 }
--- src/main/java/co/edu/ufps/emailsender/EmailController.java
+++ src/main/java/co/edu/ufps/emailsender/EmailController.java
@@ -1,25 +1,25 @@
 package co.edu.ufps.emailsender;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/email")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 public class EmailController {
 
-    @PostMapping("/send")
-    public ResponseEntity<String> sendEmail(@RequestBody EmailRequest emailRequest) {
-        System.out.println("Datos recibidos: " + emailRequest);
-
-        // Simular el envío de email con un retraso
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        // Simular éxito
-        return ResponseEntity.ok("Email enviado con éxito");
+    @GetMapping("/send")
+    public ResponseEntity<String> handleGet() {
+        return ResponseEntity.ok("Este endpoint espera una solicitud POST con un cuerpo JSON.");
     }
-}
 
+    @PostMapping("/send") 
+    public ResponseEntity<String>sendEmail(@RequestBody EmailRequest emailRequest) { 
+    	System.out.println("Datos recibidos: " + emailRequest); // Asegúrate de imprimir los datos recibidos 
+    	return ResponseEntity.ok("Email enviado con éxito"); 
+    	}
+}
